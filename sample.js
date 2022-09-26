4f27200f051b468f14cd906a46c30d58638768e0
kirlo_s <kirlo.stnu@gmail.com>
2022-09-27 01:28:54 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,47 @@
+const copyToClipboard = (function () {
+    const errorMessage = "Failed to copy to clipboard!";
+
+    function precondition() {
+        return "enabled";
+    }
+
+    async function callback(scope) {
+        try {
+            let xmlText = "";
+
+            if (selectedBlocks.length > 0) {
+                for (let i = 0; i < selectedBlocks.length; i++) {
+                    xmlText += blockToXml(selectedBlocks[i]);
+                }
+            }
+            else {
+                xmlText += blockToXml(scope.block);
+            }
+
+            await navigator.clipboard.writeText(xmlText);
+        }
+        catch (e) {
+            BF2042Portal.Shared.logError(errorMessage, e);
+
+            alert(errorMessage);
+        }
+    }
+
+    function blockToXml(block) {
+        const xmlDom = _Blockly.Xml.blockToDomWithXY(block, true);
+        _Blockly.Xml.deleteNext(xmlDom);
+
+        const xmlText = _Blockly.Xml.domToText(xmlDom).replace("xmlns=\"https://developers.google.com/blockly/xml\"", "");
+
+        return xmlText;
+    }
+
+    return {
+        id: "copyToClipboard",
+        displayText: "Copy to Clipboard",
+        scopeType: _Blockly.ContextMenuRegistry.ScopeType.BLOCK,
+        weight: 100,
+        preconditionFn: precondition,
+        callback: callback
+    };
+})();
