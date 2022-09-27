6df2b998818a565b0320efcb64a92c69ced307f2
kirlo_s <kirlo.stnu@gmail.com>
2022-09-27 11:28:41 +0900

    add register func

--- main.js
+++ main.js
@@ -1,114 +1,116 @@
 
-    const pluginName = "portal-shortcut";
+const pluginName = "portal-shortcut";
 
-    const registerShortcut = (function() {
-        const errorMessage = "Failed to register shorcut!";
-        const nameError = "Shortcut Name is invalid!";
-        function precondition(){
-            return "enabled";
-        }
+const aregisterShortcut = (function () {
+    const errorMessage = "Failed to register shorcut!";
+    const nameError = "Shortcut Name is invalid!";
 
-        function callback(scope){
-            try {
-                let xmlText = "";
-
-                if (selectedBlocks.length > 0) {
-                    for (let i = 0; i < selectedBlocks.length; i++) {
-                        xmlText += blockToXml(selectedBlocks[i]);
-                    }
-                }
-                else {
-                    xmlText += blockToXml(scope.block);
-                }
-
-
-                var entryName = prompt("Enter Shortcut Name.","");
-                if (entryName != ""){
-                    const storeData = getData();
-                    callback(storeData);
-                    var addData = {name : entryName , block : xmlText}
-                    storeData.push(addData);
-                    localStorage.setItem(pluginName,JSON.stringify(storeData))
-                }else{
-                    alert(nameError);
-                }
-            }
-            catch (e) {
-                BF2042Portal.Shared.logError(errorMessage, e);
-
-                alert(errorMessage);
-            }
-        }
-
-        function blockToXml(block) {
-            const xmlDom = _Blockly.Xml.blockToDomWithXY(block, true);
-            _Blockly.Xml.deleteNext(xmlDom);
-
-            const xmlText = _Blockly.Xml.domToText(xmlDom).replace("xmlns=\"https://developers.google.com/blockly/xml\"", "");
-
-            return xmlText;
-        }
-
-        
-        return {
-            id: "registerShortcut",
-            displayText: "Register Shortcut",
-            scopetype: _Blockly.ContextMenuRegistry.ScopeType.BLOCK,
-            weight: 100,
-            preconditionFn: precondition,
-            callback: callback
-        };
-    })();
-
-    /*
-    const deleteShortcut = (function() {
-        const errorMessage = "Failed to delete shorcut!";
-
-        function precondition(){
-            return "enabled";
-        }
-
-        return {
-            id: "deleteShortcut",
-            displayText: "Delete Shorcut",
-            scopetype: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
-            weight: 100,
-            preconditionFn: precondition,
-            callback: callback
-        };
-        
-    })();
-
-    const addFromShorcut = (function(){
-
-        function precondition(){
-            return "enabled";
-        }
-
-        return {
-            id: "addFromShortcut",
-            displayText: "Add Block from Shorcut",
-            scopetype: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
-            weight: 100,
-            preconditionFn: precondition,
-            callback: callback
-        };
-
-    })();
-    */
-
-    function getData() {
-        const storeData = localStorage.getItem(pluginName);
-
-        if (storeData) {
-            return JSON.parse(storeData);
-        }
-
-        return {};
+    function precondition() {
+        return "enabled";
     }
+
+    function callback(scope) {
+        try {
+            let xmlText = "";
+
+            if (selectedBlocks.length > 0) {
+                for (let i = 0; i < selectedBlocks.length; i++) {
+                    xmlText += blockToXml(selectedBlocks[i]);
+                }
+            }
+            else {
+                xmlText += blockToXml(scope.block);
+            }
+
+
+            var entryName = prompt("Enter Shortcut Name.", "");
+            if (entryName != "") {
+                const storeData = getData();
+                callback(storeData);
+                var addData = { name: entryName, block: xmlText }
+                storeData.push(addData);
+                localStorage.setItem(pluginName, JSON.stringify(storeData))
+            } else {
+                alert(nameError);
+            }
+        }
+        catch (e) {
+            BF2042Portal.Shared.logError(errorMessage, e);
+
+            alert(errorMessage);
+        }
+    }
+
+    function blockToXml(block) {
+        const xmlDom = _Blockly.Xml.blockToDomWithXY(block, true);
+        _Blockly.Xml.deleteNext(xmlDom);
+
+        const xmlText = _Blockly.Xml.domToText(xmlDom).replace("xmlns=\"https://developers.google.com/blockly/xml\"", "");
+
+        return xmlText;
+    }
+
+    return {
+        id: "registerShortcut",
+        displayText: "Register Shortcut",
+        scopetype: _Blockly.ContextMenuRegistry.ScopeType.BLOCK,
+        weight: 100,
+        preconditionFn: precondition,
+        callback: callback
+    };
+})();
+
+
+/*
+const deleteShortcut = (function() {
+    const errorMessage = "Failed to delete shorcut!";
+
+    function precondition(){
+        return "enabled";
+    }
+
+    return {
+        id: "deleteShortcut",
+        displayText: "Delete Shorcut",
+        scopetype: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
+        weight: 100,
+        preconditionFn: precondition,
+        callback: callback
+    };
     
-    function init(){
-        _Blockly.ContextMenuRegistry.registry.register(registerShortcut);
+})();
+
+const addFromShorcut = (function(){
+
+    function precondition(){
+        return "enabled";
     }
+
+    return {
+        id: "addFromShortcut",
+        displayText: "Add Block from Shorcut",
+        scopetype: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
+        weight: 100,
+        preconditionFn: precondition,
+        callback: callback
+    };
+
+})();
+*/
+
+function getData() {
+    const storeData = localStorage.getItem(pluginName);
+
+    if (storeData) {
+        return JSON.parse(storeData);
+    }
+
+    return {};
+}
+
+function init() {
+    _Blockly.ContextMenuRegistry.registry.register(registerShortcut);
+    console.log(scope)
+}
 
 init();
