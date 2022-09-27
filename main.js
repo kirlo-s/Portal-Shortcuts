def7cfb95a40a6df92972415eeae8675972182c4
kirlo_s <kirlo.stnu@gmail.com>
2022-09-27 15:19:09 +0900

    add register func

--- main.js
+++ main.js
@@ -23,8 +23,30 @@ const PortalShortcut = (function () {
         };
     })();
 
+    const sh = (function() {
+        function precondition() {
+            return "enabled";
+        }
+
+        function callback() {
+            console.log("splitscreen-right");
+        }
+
+        return {
+            id: "EditorBlockly",
+            displayText: "placeholder",
+            // eslint-disable-next-line no-undef
+            scopeType: _Blockly.ContextMenuRegistry.ScopeType.WORKSPACE,
+            weight: 99,
+            preconditionFn: precondition,
+            callback: callback
+        };
+    })();
+
+
     function init() {
         _Blockly.ContextMenuRegistry.registry.register(showCodeEditor);
+        _Blockly.ContextMenuRegistry.registry.register(sh);
     }
 
     init();
